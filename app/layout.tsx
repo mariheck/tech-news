f771e9178942e2d9f83962302c54a5ed9f251931
Marine Heckler <53305047+mariheck@users.noreply.github.com>
2026-05-17 17:10:08 +0200

    Wire DESIGN.md tokens through globals.css

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,11 +23,8 @@ type Props = {
 
 const RootLayout = ({ children }: Readonly<Props>) => {
   return (
-    <html
-      lang='fr'
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className='min-h-full flex flex-col'>{children}</body>
+    <html lang='fr' className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>{children}</body>
     </html>
   );
 };
